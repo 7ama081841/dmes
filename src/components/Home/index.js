5b41a2b8c97b04b5572d8dbce95d41fcc5898ae2
mohamed123 <chaabenim318@gmail.com>
2022-08-23 22:23:54 +0100

    added files

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <div className="overlay">
+                <div>
+                    <div className="container">
+                        <h1>dmes</h1>
+
+                        <div className="buttons">
+                            <button></button>
+                            <button></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
